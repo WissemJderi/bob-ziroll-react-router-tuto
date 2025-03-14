142ebc3a55b7895da52a76bcb53321acc4054b12
Wissem Jderi <asqr43641@gmail.com>
2025-03-14 12:00:04 +0100

    add Vans and Van pages; implement routing and mock server for van data

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Vans from "./pages/Vans";
+import "./server";
+import Van from "./pages/Van";
+
 function App() {
   return (
     <BrowserRouter>
@@ -10,11 +14,14 @@ function App() {
         <Link to="/">#VANLIFE</Link>
         <nav>
           <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
+        <Route path="vans/:id" element={<Van />} />
       </Routes>
     </BrowserRouter>
   );

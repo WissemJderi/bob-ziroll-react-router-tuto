27582a58d7b94133f30ee38f5ffc59e747b90347
Wissem Jderi <asqr43641@gmail.com>
2025-04-01 15:23:07 +0100

    refactor routing and component structure; move Vans and Van components to nested directories and implement Layout component

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,24 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Vans from "./pages/Vans";
+import Vans from "./pages/Vans/Vans";
 import "./server";
-import Van from "./pages/Van";
+import Van from "./pages/Vans/Van";
+import Layout from "./components/Layout";
 
+// eslint-disable-next-line react-refresh/only-export-components
 function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link to="/">#VANLIFE</Link>
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
-        </nav>
-      </header>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="vans/:id" element={<Van />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="vans/:id" element={<Van />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
